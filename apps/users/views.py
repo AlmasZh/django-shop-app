60cac57e94cb772a12297bf45821f0322277ee58
AlmasZh <almas.zh22@gmail.com>
2025-03-01 22:10:39 +0500

    add main page with tailwindcss

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from apps.users.forms import SignupForm
 
 # Create your views here.
+def index(request):
+    return render(request, 'users/index.html')
+
+
+def signup_view(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        print("\n\nform:", form, "\n\n")
+        print("\n\nform.is_valid():", form.is_valid(), "\n\n")
+        if form.is_valid():
+            user = form.save()
+            print(user)
+            login(request, user)  # Automatically log in the user after signup
+            return redirect("home")  # Redirect to home or dashboard
+    else:
+        form = SignupForm()
+
+    return render(request, "users/signup.html", {"form": form})
--- shopApp/settings.py
+++ shopApp/settings.py
@@ -15,6 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'apps/users/static',
+    BASE_DIR / 'static',
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -28,6 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
 
 # Application definition
 
