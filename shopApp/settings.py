1413bab8048bd8c879d69098d4f5df659d665525
AlmasZh <almas.zh22@gmail.com>
2025-03-01 22:11:52 +0500

    add main page with tailwindcss

--- shopApp/settings.py
+++ shopApp/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'apps/users/static',
+    # BASE_DIR / 'apps/users/static',
     BASE_DIR / 'static',
 ]
 
