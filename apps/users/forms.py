abb7924a238728b47fb2e5ee6e829e74ffec6305
AlmasZh <almas.zh22@gmail.com>
2025-03-01 22:13:00 +0500

    add signup page and its form in django

---
+++ apps/users/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from apps.users.models import CustomUser
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignupForm(forms.ModelForm):
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        required=True
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ["username", "email", "password"]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
