8d8d18821147966b8eb5acaf31da0d771a0a169a
AlmasZh <almas.zh22@gmail.com>
2025-03-05 15:37:46 +0500

    add Order and OrderItem models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from apps.users.models import CustomUser
+from apps.products.models import Product
 
 # Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='orders')
+    ordered = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f"{self.user.full_name} - order id: {self.id}"
+
+    @property
+    def get_total_price(self):
+        total = sum(item.get_cost() for item in self.items.all())
+        return total
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
+    price = models.IntegerField()
+    quantity = models.SmallIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
