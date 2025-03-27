dd8d8ddae9d7f2c452021b8c8deab8c90fb1a382
Dias <dias.aubakirov6@gmail.com>
2025-03-27 19:51:50 +0500

    add cart page front

--- shopApp/settings.py
+++ shopApp/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'shopApp.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-DATABASES = {
+DATABASESS = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'shop',
@@ -101,7 +101,7 @@ DATABASES = {
     }
 }
 
-DATABASESS = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
