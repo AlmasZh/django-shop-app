d22dbbdeee13a113d76b7aa39281bab80853cb84
Almas Zhakanov <almas.zh22@gmail.com>
2025-04-17 23:34:55 +0500

    make many fields of billingadress nullable

--- apps/payments/models.py
+++ apps/payments/models.py
@@ -12,13 +12,13 @@ ADDRESS_CHOICES = (
 class BillingAddress(models.Model):
     user = models.ForeignKey(CustomUser,
                             on_delete=models.CASCADE)
-    street_address = models.CharField(max_length=100)
-    apartment_address = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    zip = models.CharField(max_length=100)
-    address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
+    street_address = models.CharField(max_length=100, null=True, blank=True)
+    apartment_address = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    state = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    zip = models.CharField(max_length=100, null=True, blank=True)
+    address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES, default='B')
     # default = models.BooleanField(default=False)
 
     def __str__(self):
@@ -38,7 +38,7 @@ class Payment(models.Model):
     stripe_charge_id = models.CharField(max_length=50)
     user = models.ForeignKey(CustomUser,
                              on_delete=models.SET_NULL, blank=True, null=True)
-    amount = models.FloatField()
+    amount = models.FloatField(null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=10, choices=PAYMENT_STATUS_CHOICES, default='pending')
 
