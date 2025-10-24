7c5ae09d293b5bea401c1d163b703f8f4c1deaf7
AlmasZh <almas.zh22@gmail.com>
2025-10-24 18:40:49 +0500

    chore: clean code

--- apps/users/models.py
+++ apps/users/models.py
@@ -11,14 +11,14 @@ class UserManager(BaseUserManager):
 
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save()
         return user
 
     def create_superuser(self, email, password, **extra_fields):
         user = self.create_user(email, password, **extra_fields)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using=self.db)
+        user.save()
         return user
 
 
@@ -32,7 +32,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_courier = models.BooleanField(default=False)
     is_manager = models.BooleanField(default=False)
     likes = models.ManyToManyField(Product, blank=True, related_name='likes')
-
 
     objects = UserManager()
 
